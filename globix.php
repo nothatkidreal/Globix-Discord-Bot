e8b7ec27bdb92b2377d1c2042a511b80b3774b13
HatKid <143078648+nothatkidreal@users.noreply.github.com>
2024-12-13 20:08:18 +1000

    Fix copyright
    I have been advised by a lawyer to re-add my NHK Creative copyright as nothing is different in the code, so it still is my intellectual property.

--- globix.php
+++ globix.php
@@ -1,5 +1,7 @@
 <?php
+// (C) NHK Creative, 2024
 // (C) Julian QNK, 2024
+// Please refer to nothatkidreal/broadcast, everything that is different from that repository belongs to Julian QNK.
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
